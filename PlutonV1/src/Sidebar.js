66fd230f9270f51d723a4086e86d140e74770986
Avani Jain <92999202+inavaj@users.noreply.github.com>
2024-10-22 16:25:22 +0530

    Update Sidebar.js
    Sidebar with user management and logout

--- PlutonV1/src/Sidebar.js
+++ PlutonV1/src/Sidebar.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 import './Sidebar.css'; // Import the CSS file for the sidebar
 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(true);
+    const navigate = useNavigate(); // Hook for navigation
 
     const toggleSidebar = () => {
         setIsOpen(!isOpen);
@@ -22,6 +23,12 @@ const Sidebar = () => {
         handleResize(); // Initial check
         return () => window.removeEventListener('resize', handleResize);
     }, []);
+
+    // Logout function
+    const logOut = () => {
+        localStorage.setItem('token', null);
+        navigate("/"); // Redirect to the login page
+    };
 
     return (
         <>
@@ -42,10 +49,16 @@ const Sidebar = () => {
                     <li>
                         <NavLink to="/InflowOutflow" activeClassName="active" className="sidebar-link">InflowOutflow</NavLink>
                     </li>
+                    <li>
+                        <NavLink to="/User  Management" activeClassName="active" className="sidebar-link">Users Management</NavLink>
+                    </li>
                 </ul>
+                <div className="sidebar-bottom">
+                    <button className="logout-button" onClick={logOut}>Logout</button>
+                </div>
             </div>
         </>
     );
-}
+};
 
 export default Sidebar;
